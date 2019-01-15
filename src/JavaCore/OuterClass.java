24cf4bfec2ade7fd3941089920bca58c234859f1
shengxiaoyu <13314409603@163.com>
2019-01-15 09:36:49 +0800

    FloydWarshall,Proxy

---
+++ src/JavaCore/OuterClass.java
@@ -0,0 +1,28 @@
+package JavaCore;
+
+/**
+ * @author 13314409603@163.com
+ * @date 2019/1/14
+ * @time 19:55
+ * @Description
+ */
+public class OuterClass {
+    private OuterClass(){
+    }
+    private int a ;
+    private static int c ;
+    public void print(){
+        InnerClass innerClass = new InnerClass();
+        innerClass.print() ;
+        Employee employee = new Employee("test"){
+
+        } ;
+    }
+
+    private static class InnerClass{
+        public final static int b = 1 ;
+        public void print(){
+            System.out.println(c);
+        }
+    }
+}
